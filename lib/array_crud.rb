aa945679e70b6d6078f752fe68c22268fdb49baa
Chris Beyer <beyercorpuz@gmail.com>
2020-01-28 03:47:03 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -36,7 +36,7 @@ def retrieve_element_from_index(array, index_number)
   
     array[index_number]
 end
-##def retrieve_first_element_from_array(array)
+learn savedef retrieve_first_element_from_array(array)
  
  animals =  ["Dog","Cat","Rabbit","Horse"]
  array[0]
