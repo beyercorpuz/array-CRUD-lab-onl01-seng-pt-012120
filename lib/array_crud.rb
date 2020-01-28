aa597f61a6b5720db44c387d0b36c715c2f795cb
Chris Beyer <beyercorpuz@gmail.com>
2020-01-28 03:41:27 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,52 @@
-def create_an_empty_array
-  
+def 
+  create_an_empty_array
+  [ ]
 end
 
 def create_an_array
-  
+animals =  ["Dog","Cat","Rabbit","Horse"]
 end
 
 def add_element_to_end_of_array(array, element)
+  animals =  ["Dog","Cat","Rabbit","Horse"]
+  array << element
+ 
   
 end
 
 def add_element_to_start_of_array(array, element)
+  animals =  ["Dog","Cat","Rabbit","Horse"]
+  array.unshift(element) 
   
 end
 
 def remove_element_from_end_of_array(array)
+  animals =  ["Dog","Cat","Rabbit","Horse"]
+   array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
-  
+  animals =  ["Dog","Cat","Rabbit","Horse"]
+  array.shift
+ 
 end
 
 def retrieve_element_from_index(array, index_number)
   
+    [index_number]array
 end
-
-def retrieve_first_element_from_array(array)
-  
+##def retrieve_first_element_from_array(array)
+ 
+ animals =  ["Dog","Cat","Rabbit","Horse"]
+ array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
+
+end
+end
+
+
